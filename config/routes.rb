1c4f830b761db23a6594ba52de0c31bd25c40b16
Mostafa Abdelsalam <mostafa.abdelsalam96@gmail.com>
2018-07-13 01:50:58 +0200

    195

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ root 'welcome#index'
   get 'my_friends', to:"users#my_friends"
   resources :users, only:[:show]
   resources :friendships
+  get 'search_friends', to: 'users#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
