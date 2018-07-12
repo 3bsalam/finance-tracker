565a5fda3c44bc14c3f003e2d2a2369c9df7bba8
Mostafa Abdelsalam <mostafa.abdelsalam96@gmail.com>
2018-07-12 23:45:29 +0200

    189

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ root 'welcome#index'
   get 'search_stocks', to: 'stocks#search'
   resources :user_stocks, only: [:create , :destroy]
   get 'my_friends', to:"users#my_friends"
+  resources :users, only:[:show]
+  resources :friendships
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
